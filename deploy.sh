d9630675bee36563a0a443ea160765e7e00c9dce
Ben <ben7152000@gmail.com>
2024-06-17 19:45:55 +0800

    feat: 添加 github 部署

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/ben7152000/caa.github.io.git master:gh-pages
+
+cd -
