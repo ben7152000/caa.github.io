3724bda6254327dcc59528ae8d25b3bb849f6648
Ben <ben7152000@gmail.com>
2024-06-17 19:49:45 +0800

    fixed: 修改網址

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/ben7152000/caa.github.io master:gh-pages
+git push -f https://github.com/ben7152000/caa.github.io.git master:gh-pages
 
 cd -
